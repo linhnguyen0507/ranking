1b215170f81d34c5238a2a1b6eead54ce7a7ae3f
Nguyen Nhu Linh <117696227+Linhnguyen111231@users.noreply.github.com>
2023-11-20 16:45:34 +0700

    update

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class RankController extends Controller
 {
     public function getRanksArrage(){
-        return Rank::with('user:id,name')->orderBy("point","desc")->get();
+        return Rank::with('user:id,name')->orderBy("point","desc")->get()->take(10);
     }
     public function getUser(Request $request){
         $user = $request->id;
